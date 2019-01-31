b83604bc14c0a03679d1511c1b5bac5a090467eb
isaevyaroslav <iyaroslav@yandex.ru>
2019-01-31 22:44:10 +0300

    Task4

---
+++ Task4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLightModul4
+{
+    class Task4
+    {
+        /*
+         static void Main(string[] args)
+            {
+            int a = 10, b = 15, c = 0;
+            SomeMul(a, b, c);
+            char[] map = new char[15];
+            map[2] = 'T';
+            GenerateMap(map);
+            Console.WriteLine(c);//Выведет 0
+            Console.WriteLine(map[2]);//Выведет T
+            }
+            static void SomeMul(int a, int b, int c)
+            {
+            c = a * b;
+            }
+            static void GenerateMap(char[] map)
+            {
+            for(int i = 0; i < map.Length; i++)
+            {
+            map[i] = 'P';
+            }
+            }
+         */
+    }
+}
