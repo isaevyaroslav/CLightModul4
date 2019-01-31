fe678e868f05ced44bae1519c6d896f62283a42b
isaevyaroslav <iyaroslav@yandex.ru>
2019-01-31 22:55:02 +0300

    Task2

---
+++ Task2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLightModul4
+{
+    class Task2
+    {
+        /*
+         2 UIElement. 6 баллов
+            Разработайте функцию, которая рисует healthbar в определённой позиции. Она также
+            принимает некий закрашенный процент.
+            При 40% healthbar выглядит так:
+            [####_____]
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getStatLine(4, 10));
+        }
+        static string getStatLine(int statValue, int maxValue)
+        {
+            return "["+string.Empty.PadLeft(statValue, '#').PadRight(maxValue, '_')+ "]";
+        }
+    }
+}
